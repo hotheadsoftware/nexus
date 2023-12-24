a8b68c60e4e64c9ea0767fc8c9bcead3a4a894bc
Zach Mowrey <zbmowrey@gmail.com>
2023-12-24 13:28:54 -0500

    Add manage panel

--- app/Providers/Filament/ManagePanelProvider.php
+++ app/Providers/Filament/ManagePanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Manage\Resources\TenantResource\Widgets\Manage\StatsOverview;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -29,25 +30,6 @@ class ManagePanelProvider extends PanelProvider
             ->id('manage')
             ->path('manage')
             ->login()
-            ->colors(tenant() ? tenant()->getColors('manage') : [
-                'danger'  => Color::Red,
-                'primary' => Color::Amber,
-                'info'    => Color::Blue,
-                'success' => Color::Green,
-                'warning' => Color::Orange,
-                'gray'    => Color::Slate,
-            ])
-            ->brandLogo(tenant()?->logo ? asset(Storage::url("images/" . tenant()->id . "/" . tenant()->logo)) : "")
-            ->discoverResources(in: app_path('Filament/Manage/Resources'), for: 'App\\Filament\\Manage\\Resources')
-            ->discoverPages(in: app_path('Filament/Manage/Pages'), for: 'App\\Filament\\Manage\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
-            ->discoverWidgets(in: app_path('Filament/Manage/Widgets'), for: 'App\\Filament\\Manage\\Widgets')
-            ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
-            ])
             ->middleware([
                 PreventAccessFromCentralDomains::class,
                 InitializeTenancyByDomain::class,
@@ -60,6 +42,25 @@ class ManagePanelProvider extends PanelProvider
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
+            ])
+            ->colors([
+                'danger'  => Color::Red,
+                'primary' => Color::Stone,
+                'info'    => Color::Blue,
+                'success' => Color::Green,
+                'warning' => Color::Orange,
+                'gray'    => Color::Emerald,
+            ])
+            ->brandLogo(tenant()?->logo ? asset(Storage::url("images/" . tenant()->id . "/" . tenant()->logo)) : "")
+            ->discoverResources(in: app_path('Filament/Manage/Resources'), for: 'App\\Filament\\Manage\\Resources')
+            ->discoverPages(in: app_path('Filament/Manage/Pages'), for: 'App\\Filament\\Manage\\Pages')
+            ->pages([
+                Pages\Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Manage/Widgets'), for: 'App\\Filament\\Manage\\Widgets')
+            ->widgets([
+                Widgets\AccountWidget::class,
+                Widgets\FilamentInfoWidget::class,
             ])
             ->authMiddleware([
                 Authenticate::class,
