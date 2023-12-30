48fa670f36249b48f04f506ce61e95d376de6b48
Zach Mowrey <zbmowrey@gmail.com>
2023-12-30 07:50:41 -0500

    Seed Operator, not User in Tenant context

--- database/seeders/Tenant/UserSeeder.php
+++ database/seeders/Tenant/UserSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders\Tenant;
 
-use App\Models\User;
+use App\Models\Operator;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -13,14 +13,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-
         /**
          * We're only going to create this user in a local environment, but it will be created
          * with each tenant in that environment. This allows us to login to each tenant and
          * create users, etc. as needed. We'll use impersonation in prod where needed.
          */
         if (app()->environment() === 'local') {
-            User::create([
+            Operator::create([
                 'name' => config('app.app_plane.user.name'),
                 'email' => config('app.app_plane.user.email'),
                 'password' => Hash::make(config('app.app_plane.user.password')),
