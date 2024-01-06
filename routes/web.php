1be9cb32e0bc5cbf29f775a31a81843b53b3817c
Zach Mowrey <zbmowrey@gmail.com>
2024-01-06 06:51:06 -0500

    Remove commented-out test route.

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// # Test Slack Exception Message
-// Route::get('/except', function () {
-//     throw new Exception('This is an exception');
-// });
