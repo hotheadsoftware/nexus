a4b5be78c0c2d2f9bf9f644493be1d04cc657d35
Zach Mowrey <zbmowrey@gmail.com>
2024-01-08 15:57:20 -0500

    Testing pint

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+    use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use OwenIt\Auditing\Auditable;
 use Rappasoft\LaravelAuthenticationLog\Traits\AuthenticationLoggable;
