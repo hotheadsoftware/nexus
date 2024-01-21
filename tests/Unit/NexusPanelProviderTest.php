f8d543244e1fb5ba365d351f1ba8df8ee31bc1d0
Zach Mowrey <zbmowrey@gmail.com>
2024-01-21 09:42:55 -0500

    clarify test naming & provide comments

--- tests/Unit/NexusPanelProviderTest.php
+++ tests/Unit/NexusPanelProviderTest.php
@@ -7,11 +7,17 @@
  *
  * If this test sequence fails, it means that the stub file has been updated and
  * we need to update our replacement logic in app/Console/Commands/NexusMakePanel.php
+ *
+ * Troubleshooting: delete the vendor/ folder and re-run composer install. If that
+ * doesn't work, we've probably encountered a breaking change in Filament. We'll
+ * need to update the stub file and the stub manipulation logic found in our
+ * NexusMakePanelProviderStub.php file.
  */
+
 $stubFilePath    = 'vendor/filament/support/stubs/PanelProvider.stub';
 $stubFileContent = file_get_contents($stubFilePath);
 
-it('has a filament panel provider stub file', function () use ($stubFilePath) {
+it('has the stub file in the right location', function () use ($stubFilePath) {
 
     $this->assertTrue(file_exists($stubFilePath));
 });
@@ -29,7 +35,7 @@ it('has the necessary strings in place for us to find & replace', function () us
     }
 });
 
-it('looks like a default panel provider stub (not already nexus-replaced)', function () use ($stubFileContent) {
+it('looks like a new stub', function () use ($stubFileContent) {
     $find = [
         '# Custom Branding Goes Here',
         'PreventAccessFromCentralDomains::class',
