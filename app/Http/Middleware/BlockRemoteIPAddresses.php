715b019efa009b2fcdaea032e2cccea43583aa72
Zach Mowrey <zbmowrey@gmail.com>
2024-02-23 21:17:23 -0500

    Remote IP Block Functionality

---
+++ app/Http/Middleware/BlockRemoteIPAddresses.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class BlockRemoteIPAddresses
+{
+    public function handle($request, $next)
+    {
+        /**
+         * If the setting is false, we perform no blocking here.
+         */
+        if(!config('app.block_remote_ips')) {
+            return $next($request);
+        }
+
+        /**
+         * List of routes which should always be allowed to reach any endpoint.
+         */
+        $allowedRoutes = config('app.allowed_routes', []);
+
+        /**
+         * List of IP addresses which should always be allowed to reach any endpoint.
+         */
+        $allowedIPAddresses = config('app.allowed_ips', []);
+
+        if(!in_array($request->ip(), $allowedIPAddresses)) {
+            if (! in_array($request->route()->getName(), array_values($allowedRoutes))) {
+                exit;
+            }
+        }
+
+        return $next($request);
+    }
+}
